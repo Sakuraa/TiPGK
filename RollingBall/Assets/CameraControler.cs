ef7ba2146b4208f8993517a1fcd518849696c2c2
Paulina Cebo <Paulina Cebo>
2016-05-17 00:18:50 +0200

    level 1 PoC

---
+++ RollingBall/Assets/CameraControler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+
+    public Transform sphere;
+
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vector = new Vector3(0, 3f, -5f);
+
+        Rigidbody rigidbody = sphere.GetComponent<Rigidbody>();
+        float velocity = rigidbody.velocity.sqrMagnitude;
+
+        vector = vector * (1f + velocity / 254);
+
+        Vector3 newPosition = sphere.position + vector;
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.02f);
+
+        transform.position = sphere.position + vector;
+        transform.LookAt(sphere);
+    }
+}
