5b5a79dec1263755ba9d097023278487f78bf6c5
Paulina Cebo <Paulina Cebo>
2016-05-22 02:14:28 +0200

    level 3

---
+++ RollingBall/Assets/scripts/Intro.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+	IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(2.5f);
+        //SceneManager.LoadScene("The little big ball");
+        SceneManager.LoadScene("LEVEL 1");
+	}
+}
